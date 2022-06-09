1b078744aa23b8b553ca801be1821f34e30c0db3
Arpit Agrawal <78686299+arpitagrawal24@users.noreply.github.com>
2022-06-09 16:15:57 +0530

    Time: 104 ms (37.55%), Space: 61.6 MB (68.93%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -4,12 +4,12 @@ class Solution
         bool increasingTriplet(vector<int> &nums)
         {
             int c1 = INT_MAX, c2 = INT_MAX;
-            for (auto it: nums)
+            for (auto i: nums)
             {
-                if (it <= c1)
-                    c1 = it;
-                else if (it <= c2)
-                    c2 = it;
+                if (i <= c1)
+                    c1 = i;
+                else if (i <= c2)
+                    c2 = i;
                 else
                     return true;
             }
