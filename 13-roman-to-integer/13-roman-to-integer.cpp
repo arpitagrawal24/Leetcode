8e8c5908427e91ebf0ba50aa3afdcd6a1bc55b47
Arpit Agrawal <78686299+arpitagrawal24@users.noreply.github.com>
2022-09-16 16:05:42 +0530

    Time: 19 ms (55.40%), Space: 8.9 MB (8.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,84 +1,15 @@
-class Solution
-{
-    public:
-        int romanToInt(string s)
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        unordered_map<char,int> m { {'I',1} , {'V',5} , {'X',10} , {'L',50} , {'C',100} , {'D',500} , {'M',1000} };
+        for(int i = 0; i < s.size() ; i++)
         {
-            int ans = 0;
-            for (int i = 0; i < s.size(); i++)
-            {
-                switch (s[i])
-                {
-                    case 'I':
-                    {
-                        if (s[i + 1] == 'V')
-                        {
-                            ans += 4;
-                            i++;
-                        }
-                        else if (s[i + 1] == 'X')
-                        {
-                            ans += 9;
-                            i++;
-                        }
-                        else
-                        {
-                            ans += 1;
-                        }
-                        break;
-                    }
-                    case 'V':
-                        ans += 5;
-                        break;
-                    case 'X':
-                    {
-                        if (s[i + 1] == 'L')
-                        {
-                            ans += 40;
-                            i++;
-                        }
-                        else if (s[i + 1] == 'C')
-                        {
-                            ans += 90;
-                            i++;
-                        }
-                        else
-                        {
-                            ans += 10;
-                        }
-                        break;
-                    }
-
-                    case 'L':
-                        ans += 50;
-                        break;
-                    case 'C':
-                    {
-                        if (s[i + 1] == 'D')
-                        {
-                            ans += 400;
-                            i++;
-                        }
-                        else if (s[i + 1] == 'M')
-                        {
-                            ans += 900;
-                            i++;
-                        }
-                        else
-                        {
-                            ans += 100;
-                        }
-                        break;
-                    }
-                    case 'D':
-                        ans += 500;
-                        break;
-                    case 'M':
-                        ans += 1000;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return ans;
+            if(m[s[i]] < m[s[i+1]])
+                ans -=m[s[i]];
+            else
+                ans += m[s[i]];
         }
+        return ans;
+    }
 };
