a05edea18442f70f02826de9e453b3a5f9ae4ed1
Arpit Agrawal <78686299+arpitagrawal24@users.noreply.github.com>
2022-09-21 17:27:35 +0530

    Time: 36 ms (15.17%), Space: 21.4 MB (77.30%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -1,36 +1,35 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-    int n;
 
-    TreeNode* sortedArrayToBST(vector<int> &nums)
-    {
-        n = nums.size() - 1;
-        TreeNode *tree = NULL;
-        tree = make_bst(nums, tree, 0, n);
-        return tree;
-    }
+        TreeNode* BST(vector<int> &nums, int i, int j)
+        {
+            if (i > j)
+                return NULL;
+            
+                int mid = i + (j - i) / 2;
+                TreeNode *root = new TreeNode(nums[mid]);
 
-    TreeNode* make_bst(vector<int> &nums, TreeNode *tree, int start, int end)
-    {
-        if (start > end || start < 0 || end > n) return NULL;
+                root->left = BST(nums, i, mid - 1);
+                root->right = BST(nums, mid + 1, j);
 
-        int mid = (start + end) / 2;
-        tree = new TreeNode(nums[mid]);
+                return root;
+        }
 
-        tree->left = make_bst(nums, tree->left, start, mid - 1);
-        tree->right = make_bst(nums, tree->right, mid + 1, end);
-        return tree;
-    }
+        TreeNode* sortedArrayToBST(vector<int> &nums)
+        {
+            int i = 0, j = nums.size() - 1;
+            return BST(nums, i, j);
+        }
 };
